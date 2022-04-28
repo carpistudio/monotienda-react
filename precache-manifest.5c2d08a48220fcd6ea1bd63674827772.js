0f183d7098eda69f27a4bdcfebd9ceceb8736492
carpinx <colettamatias@gmail.com>
2022-04-28 10:49:05 -0300

    Updates

---
+++ precache-manifest.5c2d08a48220fcd6ea1bd63674827772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a02087f043667b681903",
+    "url": "/monotienda-react/static/css/main.59e27688.chunk.css"
+  },
+  {
+    "revision": "a02087f043667b681903",
+    "url": "/monotienda-react/static/js/main.879d2d5d.chunk.js"
+  },
+  {
+    "revision": "980bae7a51821301e600",
+    "url": "/monotienda-react/static/js/runtime~main.35f5b4c2.js"
+  },
+  {
+    "revision": "b49d40691e500df3da6e",
+    "url": "/monotienda-react/static/js/2.59884771.chunk.js"
+  },
+  {
+    "revision": "5097f7748032a6907a24",
+    "url": "/monotienda-react/static/js/3.340461a2.chunk.js"
+  },
+  {
+    "revision": "3ca51fe52176796fe5664791bd6e72cf",
+    "url": "/monotienda-react/index.html"
+  }
+];
